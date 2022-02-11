574a286778a2c96e5cef1bdb4331126e3349b44d
OueslatiHassen <hassen.oueslati@esprit.tn>
2022-02-11 21:35:27 +0100

    dashbord admin customization

--- hub/admin.py
+++ hub/admin.py
@@ -1,5 +1,80 @@
+from re import search
 from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register([Student,Coach,Projet,MemberShip])
+class ProjectInline(admin.TabularInline):
+    model = Projet
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+    'last_name',
+    'first_name'
+    )
+    fields = (
+        ('last_name', 'first_name'),
+        'email'
+    )
+    search_fields = ['last_name']
+    #pour ajouter une autre critaire de recherche ['last_name',...]
+
+    inlines = [ProjectInline]
+
+class CoachAdmin(admin.ModelAdmin):
+    list_display = (
+    'last_name',
+    'first_name'
+    )
+    fields = (
+        ('last_name', 'first_name'),
+        'email'
+    )
+    search_fields = ['first_name']
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'project_name',
+        'dure',
+        'supervisor',
+        'creator',
+        'isValid'
+    )
+    fieldsets = [
+        (
+            None ,
+            {
+                'fields': ('isValid',)
+            }
+        ),
+        (
+            'About',
+            {
+                'fields': (
+                    'project_name',
+                    ('creator', 'supervisor'),
+                    'besoin',
+                    'description'
+                    )
+            }
+        ),
+        (
+            'Durations',
+            {
+                'classes':('collapse',),
+                'fields' : (
+                    'dure',
+                    'temp_allocated'
+                )
+            }
+        )
+    ]
+    #radio_fields = {'supervisor' : admin.VERTICAL}
+    autocomplete_fields = ['supervisor']
+    empty_value_display = '-empty'
+    #readonly_fields=('besoin')
+
+#admin.site.register(Student, StudentAdmin)
+admin.site.register(Coach, CoachAdmin)
+admin.site.register(Projet, ProjectAdmin)
+admin.site.register(MemberShip)
