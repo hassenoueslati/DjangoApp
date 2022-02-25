a6577bdc8884c4c71fabb6665e63be999ba3b7cb
OueslatiHassen <hassen.oueslati@esprit.tn>
2022-02-25 20:08:59 +0100

    add routing & template & StudentListView

--- hub/urls.py
+++ hub/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import homePage, student_details, students_coach_List
+from .views import homePage, student_details, students_coach_List, StudentListView
 
 urlpatterns = [
     path('home/', homePage, name="home"),
     path('index/', students_coach_List, name= 'index'),
     path('index/<int:id>', student_details, name= 'detailstudent'),
+    path('ListStudent', StudentListView.as_view(), name= 'student'),
 
 ]
--- hub/views.py
+++ hub/views.py
@@ -1,5 +1,7 @@
+from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 
 from hub.models import Coach, Student
 
@@ -19,6 +21,14 @@ def students_coach_List(request):
             'coachs' :list2 ,
         }
     )
+class StudentListView(ListView):
+    model=Student
+    template_name="hub/index.html"
+    paginate_by = 5
+
+class StudentDetailView(DetailView):
+    model=Student
+
 
 def student_details(request, id):
     student = Student.objects.get(id=id)
